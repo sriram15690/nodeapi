19f0a70fb76861378a0a2501facf0be5dec1c83f
Sriram Ramakrishnan <sriram.ramakrishnan3@cognizant.com>
2016-05-11 16:53:34 +0800

    Procfile & Port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static(__dirname+'/assets/'));
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs')
 var binPath = phantomjs.path
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname+'/assets/'));
+
 
 app.get('/', function (req, res) {
    res.sendFile(__dirname+'/assets/templates/index.html');
@@ -22,6 +24,6 @@ app.get('/dbsrate', function (req, res) {
 
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Example app listening on port '+app.get('port'));
 });
